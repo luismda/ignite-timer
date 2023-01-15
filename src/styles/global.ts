31d675436816bdc4d0531620a555f131fb8da4ce
luismda <luisdutra2004@gmail.com>
2023-01-15 16:35:08 -0300

    Initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+  }
+
+  body {
+    background: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }  
+
+  @media (max-width: 1366px) {
+    html {
+      font-size: 93.5%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+`
